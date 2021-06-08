068149f7e9d9dee465b7ac966839be9190660cc9
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-08 16:07:15 +0100

    update index

--- index.php
+++ index.php
@@ -202,7 +202,7 @@
   </footer>
   <!-- Scripts -->
   <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/5.0.0/js/bootstrap.min.js"> </script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/owl-carousel.js"></script>
   <script src="assets/js/animation.js"></script>
   <script src="assets/js/imagesloaded.js"></script>
